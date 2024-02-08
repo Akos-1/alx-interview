cf048551ef82634140719094778535d7182b9e0f
Akos-1 <veronicagyasi@gmail.com>
2024-02-07 20:57:59 -0800

    errors fixed

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -13,9 +13,9 @@ def pascal_triangle(n):
         return []
 
     triangle = [[1]]
-    for i in range(1, n):
+    for _ in range(1, n):
         prev_row = triangle[-1]
-        new_row = [1] + [prev-row[i] + prev_row[i + 1] for j
+        new_row = [1] + [prev_row[i] + prev_row[i + 1] for i
                          in range(len(prev_row) - 1)] + [1]
         triangle.append(new_row)
 
