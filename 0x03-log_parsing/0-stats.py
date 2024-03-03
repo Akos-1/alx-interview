280546e633c0dc1c4673e6acf6eb7077bfb9e16c
Akos-1 <veronicagyasi@gmail.com>
2024-03-03 01:33:07 -0800

    worked on

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,20 +3,20 @@
 import sys
 
 
-def print_statistics(status_codes, total_file_size):
+def print_message(dict_status_code, total_file_size):
     """
     Data to be printed
     Args:
-        status_codes: dictionary of status codes
+        dict_status_code: dictionary of status codes
         total_file_size: the total file
     Returns:
         nothing
     """
 
     print("File size: {}".format(total_file_size))
-    for key, count in sorted(staus_codes.items()):
-        if count != 0:
-            print("{}: {}".format(key, count))
+    for key, val in sorted(dict_staus_code.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
 
 
 total_file_size = 0
@@ -43,11 +43,11 @@ try:
                 total_file_size += int(parsed_line[0])
                 code = parsed_line[1]
 
-                if (code in status_code.keys()):
+                if (code in dict_status_code.keys()):
                     status_code[code] += 1
                 if (line_count == 10):
-                    print_statistics(status_codes, total_file_size)
+                    print_message(dict_status_code, total_file_size)
                     line count = 0
 
 finally:
-    print_statistics(status_codes, total_file_size)
+    print_message(dict_status_code, total_file_size)
