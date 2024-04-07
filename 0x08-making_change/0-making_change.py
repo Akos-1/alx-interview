3a2cd893101e602cda6d05c0aa079e791a6862f7
Akos-1 <veronicagyasi@gmail.com>
2024-04-07 12:23:47 -0700

    updated

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""a pile of coins of different values"""
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+
+    # Create a list to store the fewest number of coins needed for each total
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    # Iterate over each coin value
+    for coin in coins:
+        # Update the fewest number of coins needed for each total
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    if dp[total] == float('inf'):
+        return -1
+    else:
+        return dp[total]
